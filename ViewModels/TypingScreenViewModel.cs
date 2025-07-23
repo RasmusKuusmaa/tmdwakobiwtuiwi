077c37d83c3918131afb4c683097c88f39f56401
Rasmus Kuusmaa <77f315@gmail.com>
2025-07-23 23:28:07 +0200

    time over

--- ViewModels/TypingScreenViewModel.cs
+++ ViewModels/TypingScreenViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Threading;
+
 namespace tmdwa.ViewModels
 {
     internal class TypingScreenViewModel : BaseViewModel
     {
-        private bool _isAwaitingRestart = false;
+        private bool _isSessionActive = false;
+        private bool _isProgrammaticChange = false;
         private string _writtenText = string.Empty;
+
         public string WrittenText
         {
             get => _writtenText;
@@ -20,48 +23,71 @@ namespace tmdwa.ViewModels
                 }
             }
         }
-        private readonly DispatcherTimer _timer;
+
+        private readonly DispatcherTimer _timeoutTimer;
+        private readonly DispatcherTimer _writingTimer;
         private readonly TimeSpan _timeout = TimeSpan.FromSeconds(2);
-        private bool _isProgrammaticChange = false;
+        private readonly TimeSpan _writingTime = TimeSpan.FromSeconds(5);
+
         public TypingScreenViewModel()
         {
-            _timer = new DispatcherTimer
-            {
-                Interval = _timeout,
-            };
-            _timer.Tick += TimerElapsed;
+            _timeoutTimer = new DispatcherTimer { Interval = _timeout };
+            _timeoutTimer.Tick += OnTimeout;
+
+            _writingTimer = new DispatcherTimer { Interval = _writingTime };
+            _writingTimer.Tick += OnWritingTimeOver;
         }
-        private void TimerElapsed(object sender, EventArgs e)
+
+        private void OnTimeout(object sender, EventArgs e)
         {
-            if (_isAwaitingRestart)
-                return;
-            _timer.Stop();
-            _isAwaitingRestart = true;
+            StopSession();
             _isProgrammaticChange = true;
             WrittenText = string.Empty;
             _isProgrammaticChange = false;
             MessageBox.Show("typing has been stopped, progress has been lost", "Times up", MessageBoxButton.OK, MessageBoxImage.Warning);
         }
-        private void ResetTimer()
+
+        private void OnWritingTimeOver(object sender, EventArgs e)
         {
-            _timer.Stop();
-            _timer.Start();
+            StopSession();
+            MessageBox.Show("time over");
         }
+
+        private void StartSession()
+        {
+            _isSessionActive = true;
+            _timeoutTimer.Start();
+            _writingTimer.Start();
+        }
+
+        private void StopSession()
+        {
+            _isSessionActive = false;
+            _timeoutTimer.Stop();
+            _writingTimer.Stop();
+        }
+
+        private void ResetTimeoutTimer()
+        {
+            if (_isSessionActive)
+            {
+                _timeoutTimer.Stop();
+                _timeoutTimer.Start();
+            }
+        }
+
         private void OnUserTyped()
         {
             if (_isProgrammaticChange)
                 return;
-            if (_isAwaitingRestart)
+
+            if (!_isSessionActive)
             {
-                _isAwaitingRestart = false;
-                _timer.Start();
+                StartSession();
             }
             else
             {
-                if (!_timer.IsEnabled)
-                    _timer.Start();
-                else
-                    ResetTimer();
+                ResetTimeoutTimer();
             }
         }
     }
