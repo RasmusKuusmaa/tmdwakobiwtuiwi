415e0021cde1c6c710321af5acd85af16d70338e
Rasmus Kuusmaa <77f315@gmail.com>
2025-07-23 23:56:22 +0200

    customization

--- ViewModels/TypingScreenViewModel.cs
+++ ViewModels/TypingScreenViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Threading;
+using System.Windows.Media;
 
 namespace tmdwa.ViewModels
 {
@@ -9,6 +10,18 @@ namespace tmdwa.ViewModels
         private bool _isSessionActive = false;
         private bool _isProgrammaticChange = false;
         private string _writtenText = string.Empty;
+        private int _sessionTimeLeft;
+        private int _timeoutTimeLeft;
+        private int _sessionDuration = 300;
+        private int _timeoutDuration = 5;
+        private double _timeoutDelay = 1.0;
+        private bool _colorWarningEnabled = true;
+        private double _colorWarningThreshold = 2.5;
+        private string _warningColorName = "Red";
+        private bool _sessionTimeInMinutes = true;
+        private Brush _timeoutTimerColor = Brushes.Black;
+        private Brush _textColor = Brushes.Black;
+        private double _timeoutCountdownDelay = 0;
 
         public string WrittenText
         {
@@ -24,18 +37,223 @@ namespace tmdwa.ViewModels
             }
         }
 
+        public int SessionTimeLeft
+        {
+            get => _sessionTimeLeft;
+            set
+            {
+                _sessionTimeLeft = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int TimeoutTimeLeft
+        {
+            get => _timeoutTimeLeft;
+            set
+            {
+                _timeoutTimeLeft = value;
+                OnPropertyChanged();
+                TimeoutTimerColor = value == 1 ? Brushes.Red : Brushes.Black;
+            }
+        }
+
+        public int SessionDuration
+        {
+            get => _sessionDuration;
+            set
+            {
+                if (value > 0)
+                {
+                    _sessionDuration = value;
+                    if (!_isSessionActive)
+                        SessionTimeLeft = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int TimeoutDuration
+        {
+            get => _timeoutDuration;
+            set
+            {
+                if (value > 0)
+                {
+                    _timeoutDuration = value;
+                    if (!_isSessionActive)
+                        TimeoutTimeLeft = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public double TimeoutDelay
+        {
+            get => _timeoutDelay;
+            set
+            {
+                if (value >= 0)
+                {
+                    _timeoutDelay = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool ColorWarningEnabled
+        {
+            get => _colorWarningEnabled;
+            set
+            {
+                _colorWarningEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double ColorWarningThreshold
+        {
+            get => _colorWarningThreshold;
+            set
+            {
+                if (value >= 0)
+                {
+                    _colorWarningThreshold = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string WarningColorName
+        {
+            get => _warningColorName;
+            set
+            {
+                _warningColorName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool SessionTimeInMinutes
+        {
+            get => _sessionTimeInMinutes;
+            set
+            {
+                _sessionTimeInMinutes = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(DisplaySessionDuration));
+            }
+        }
+
+        public double DisplaySessionDuration
+        {
+            get => _sessionTimeInMinutes ? _sessionDuration / 60.0 : _sessionDuration;
+            set
+            {
+                var newDuration = _sessionTimeInMinutes ? (int)(value * 60) : (int)value;
+                if (newDuration > 0)
+                {
+                    SessionDuration = newDuration;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public Brush TimeoutTimerColor
+        {
+            get => _timeoutTimerColor;
+            set
+            {
+                _timeoutTimerColor = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Brush TextColor
+        {
+            get => _textColor;
+            set
+            {
+                _textColor = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private readonly DispatcherTimer _displayTimer;
         private readonly DispatcherTimer _timeoutTimer;
         private readonly DispatcherTimer _writingTimer;
-        private readonly TimeSpan _timeout = TimeSpan.FromSeconds(2);
-        private readonly TimeSpan _writingTime = TimeSpan.FromSeconds(5);
 
         public TypingScreenViewModel()
         {
-            _timeoutTimer = new DispatcherTimer { Interval = _timeout };
+            _displayTimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            _displayTimer.Tick += OnDisplayTimerTick;
+
+            _timeoutTimer = new DispatcherTimer();
             _timeoutTimer.Tick += OnTimeout;
 
-            _writingTimer = new DispatcherTimer { Interval = _writingTime };
+            _writingTimer = new DispatcherTimer();
             _writingTimer.Tick += OnWritingTimeOver;
+
+            SessionTimeLeft = _sessionDuration;
+            TimeoutTimeLeft = _timeoutDuration;
+        }
+
+        private void OnDisplayTimerTick(object sender, EventArgs e)
+        {
+            if (_isSessionActive)
+            {
+                SessionTimeLeft--;
+
+                if (_timeoutCountdownDelay > 0)
+                {
+                    _timeoutCountdownDelay -= 1.0;
+                }
+                else
+                {
+                    TimeoutTimeLeft--;
+
+                    if (_colorWarningEnabled && TimeoutTimeLeft <= _colorWarningThreshold)
+                    {
+                        var warningBrush = GetBrushFromColorName(_warningColorName);
+                        TimeoutTimerColor = warningBrush;
+                        TextColor = warningBrush;
+                    }
+                    else
+                    {
+                        TimeoutTimerColor = Brushes.Black;
+                        TextColor = Brushes.Black;
+                    }
+                }
+
+                if (SessionTimeLeft <= 0)
+                {
+                    OnWritingTimeOver(sender, e);
+                }
+                else if (TimeoutTimeLeft <= 0)
+                {
+                    OnTimeout(sender, e);
+                }
+            }
+        }
+
+        private Brush GetBrushFromColorName(string colorName)
+        {
+            return colorName.ToLower() switch
+            {
+                "red" => Brushes.Red,
+                "blue" => Brushes.Blue,
+                "green" => Brushes.Green,
+                "orange" => Brushes.Orange,
+                "purple" => Brushes.Purple,
+                "yellow" => Brushes.Yellow,
+                "pink" => Brushes.Pink,
+                "brown" => Brushes.Brown,
+                "gray" => Brushes.Gray,
+                "darkred" => Brushes.DarkRed,
+                "darkblue" => Brushes.DarkBlue,
+                "darkgreen" => Brushes.DarkGreen,
+                _ => Brushes.Red
+            };
         }
 
         private void OnTimeout(object sender, EventArgs e)
@@ -56,6 +274,16 @@ namespace tmdwa.ViewModels
         private void StartSession()
         {
             _isSessionActive = true;
+            SessionTimeLeft = _sessionDuration;
+            TimeoutTimeLeft = _timeoutDuration;
+            _timeoutCountdownDelay = _timeoutDelay;
+            TimeoutTimerColor = Brushes.Black;
+            TextColor = Brushes.Black;
+
+            _timeoutTimer.Interval = TimeSpan.FromSeconds(_timeoutDuration);
+            _writingTimer.Interval = TimeSpan.FromSeconds(_sessionDuration);
+
+            _displayTimer.Start();
             _timeoutTimer.Start();
             _writingTimer.Start();
         }
@@ -63,14 +291,24 @@ namespace tmdwa.ViewModels
         private void StopSession()
         {
             _isSessionActive = false;
+            _displayTimer.Stop();
             _timeoutTimer.Stop();
             _writingTimer.Stop();
+
+            SessionTimeLeft = _sessionDuration;
+            TimeoutTimeLeft = _timeoutDuration;
+            TimeoutTimerColor = Brushes.Black;
+            TextColor = Brushes.Black;
         }
 
         private void ResetTimeoutTimer()
         {
             if (_isSessionActive)
             {
+                TimeoutTimeLeft = _timeoutDuration;
+                _timeoutCountdownDelay = _timeoutDelay;
+                TimeoutTimerColor = Brushes.Black;
+                TextColor = Brushes.Black;
                 _timeoutTimer.Stop();
                 _timeoutTimer.Start();
             }
---
+++ Views/BoolToIntConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace tmdwa.Views
+{
+    public class BoolToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return boolValue ? 1 : 0;
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int intValue)
+                return intValue == 1;
+            return false;
+        }
+    }
+}
